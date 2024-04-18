404b24625f6890cda52cfff4e75ea64553e62bbf
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-04-18 14:24:13 +0200

    Added files for project setup

---
+++ OpenWeather API/Model/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  OpenWeather API
+//
+//  Created by Nizaam Haffejee on 2024/04/18.
+//
+
+import Foundation
---
+++ OpenWeather API/Web Services/Weather Web Service.swift
@@ -0,0 +1,8 @@
+//
+//  Weather Web Service.swift
+//  OpenWeather API
+//
+//  Created by Nizaam Haffejee on 2024/04/18.
+//
+
+import Foundation
