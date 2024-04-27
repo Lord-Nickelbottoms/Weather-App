69b7146d70ae8a7acc4eb0b3b79f29d7389e658a
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-04-27 15:27:03 +0200

    Renamed file and class

--- OpenWeather API/View/WeatherViewController.swift
+++ OpenWeather API/View/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  OpenWeather API
 //
 //  Created by Nizaam Haffejee on 2024/04/18.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
