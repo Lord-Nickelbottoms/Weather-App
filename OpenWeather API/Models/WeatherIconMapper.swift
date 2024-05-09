ee90a8ecb06e57204ef8c30bace4168db7677dea
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-05-09 17:37:49 +0200

    default switch case returns a custom image/icon

--- OpenWeather API/Models/WeatherIconMapper.swift
+++ OpenWeather API/Models/WeatherIconMapper.swift
@@ -51,7 +51,7 @@ struct WeatherIconMapper {
                 return "cloud"
                 
             default:
-                return "error"
+                return "cloud.slash"
         }
     }
 }
